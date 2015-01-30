d22ac30627f3a15d5c2c4b5cf41a5ed1eab969cb
Florent CHAUVEAU <florent.chauveau+github@gmail.com>
2015-01-30 14:05:06 +0100

    Import files

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClockResIcon
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            NotifyIcon nfIcon = new NotifyIcon();
+            nfIcon.Icon = GetIcon();
+            nfIcon.Visible = true;
+            nfIcon.MouseClick += delegate { Application.Exit(); };
+            
+            Timer t = new Timer();
+            t.Interval = 10000;
+            t.Tick += delegate { nfIcon.Icon = GetIcon(); };
+            t.Start();
+
+            Application.Run();
+
+            nfIcon.Visible = false;
+        }
+
+        public static Icon GetIcon()
+        {
+            var caps = WinApiCalls.QueryTimerResolution();
+            var number = (int)(caps.PeriodCurrent / 10000.0);
+
+            Bitmap bitmap = new Bitmap(32, 32);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            Font font = new Font("Consolas", 16, FontStyle.Bold);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            SolidBrush fill = new SolidBrush(Color.Black);
+            if (number < 5)
+            {
+                fill.Color = Color.Red;
+            } else if (number < 10)
+            {
+                fill.Color = Color.DarkOrange;
+            } else if (number < 15)
+            {
+                fill.Color = Color.Orange;
+            }
+
+            graphics.FillRectangle(fill, 0, 0, bitmap.Height, bitmap.Width);
+            graphics.DrawString(number.ToString(), font, new SolidBrush(Color.White), 16, 2, format);
+            Icon createdIcon = Icon.FromHandle(bitmap.GetHicon());
+
+            fill.Dispose();
+            font.Dispose();
+            graphics.Dispose();
+            bitmap.Dispose();
+
+            return createdIcon;
+        }
+    }
+}
---
+++ WinApiCalls.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * This is based on https://github.com/tebjan/TimerTool (Tebjan Halm)
+ * Thank you :)
+ */
+
+using System.Runtime.InteropServices;
+
+
+namespace ClockResIcon
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TimerCaps
+    {
+        public uint PeriodMin;
+        public uint PeriodMax;
+        public uint PeriodCurrent;
+    };
+
+    /// <summary>
+    /// Description of WinApiCalls.
+    /// </summary>
+    public static class WinApiCalls
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtQueryTimerResolution(out uint MinimumResolution, out uint MaximumResolution, out uint ActualResolution);
+
+        public static TimerCaps QueryTimerResolution()
+        {
+            var caps = new TimerCaps();
+            NtQueryTimerResolution(out caps.PeriodMin, out caps.PeriodMax, out caps.PeriodCurrent);
+            return caps;
+        }
+    }
+}
