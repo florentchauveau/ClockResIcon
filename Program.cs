964b0fd3426783466e2e6ca205478bd3d969531c
Florent CHAUVEAU <florent.chauveau+github@gmail.com>
2015-02-06 16:11:49 +0100

    Refactor to non static

--- Program.cs
+++ Program.cs
@@ -4,11 +4,42 @@ using System.Windows.Forms;
 
 namespace ClockResIcon
 {
-    static class Program
+    class Program
     {
-        private static Timer timer = new Timer();
-        private static int[] timers = new int[] { 1, 10, 30 };
-        private static NotifyIcon nfIcon = new NotifyIcon();
+        /* Refresh icon timer */
+        private Timer timer = new Timer();
+        /* Refresh timers (context menu) */
+        private int[] refreshTimers = new int[] { 1, 10, 30 };
+        /* Notification area icon */
+        private NotifyIcon nfIcon = new NotifyIcon();
+
+        public Program()
+        {
+            nfIcon.ContextMenuStrip = CreateContextMenu();
+            nfIcon.Icon = GetIcon();
+            nfIcon.Visible = true;
+            /* Invoke a 'click' on the first item of the context menu */
+            (nfIcon.ContextMenuStrip.Items[0] as ToolStripMenuItem).PerformClick();
+
+            /* Timer tick: update nfIcon */
+            timer.Tick += delegate {
+                try
+                {
+                    nfIcon.Icon = GetIcon();
+                }
+                catch (Exception)
+                {
+                    /* occasionally, there is a GDI+ exception. For now, we ignore it. */
+        }
+            };
+        }
+
+        public void Run()
+        {
+            Application.Run();
+            /* When application is done, hide nfIcon */
+            nfIcon.Visible = false;
+        }
 
         /// <summary>
         /// The main entry point for the application.
@@ -16,46 +47,50 @@ namespace ClockResIcon
         [STAThread]
         static void Main()
         {
-            nfIcon.Icon = GetIcon();
-            nfIcon.Visible = true;
-            nfIcon.ContextMenuStrip = new ContextMenuStrip();
-            foreach (int interval in timers)
-            {
-                ToolStripMenuItem item = new ToolStripMenuItem("Timer: " + interval.ToString() + "s");
-                item.Name = "timer";
-                item.Click += TimerItemClick;
-                item.Tag = interval;
-                nfIcon.ContextMenuStrip.Items.Add(item);
-            }
-            nfIcon.ContextMenuStrip.Items.Add("-");
-            nfIcon.ContextMenuStrip.Items.Add("Exit", null, delegate { Application.Exit(); });
-
-            (nfIcon.ContextMenuStrip.Items[0] as ToolStripMenuItem).PerformClick();
-
-            timer.Tick += delegate { nfIcon.Icon = GetIcon(); };
-
-            Application.Run();
-
-            nfIcon.Visible = false;
+            /* Run baby run! */
+            new Program().Run();
         }
 
-        private static void TimerItemClick(object sender, EventArgs e)
+        /* Create the icon context menu */
+        private ContextMenuStrip CreateContextMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+            foreach (int interval in refreshTimers)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem("Refresh: " + interval.ToString() + "s");
+                item.Name = "refresh";
+                item.Click += RefreshItemClick;
+                item.Tag = interval;
+                menu.Items.Add(item);
+            }
+            menu.Items.Add("-");
+            menu.Items.Add("Exit", null, delegate { Application.Exit(); });
+            return menu;
+        }
+
+        private void RefreshItemClick(object sender, EventArgs e)
         {
             ToolStripMenuItem item = sender as ToolStripMenuItem;
+            /* Update the timer interval */
             timer.Interval = (int) item.Tag * 1000;
+            /* Uncheck menu items */
             foreach (ToolStripItem i in nfIcon.ContextMenuStrip.Items)
             {
-                if (i.Name == "timer") (i as ToolStripMenuItem).Checked = false;
+                if (i.Name == "refresh") (i as ToolStripMenuItem).Checked = false;
             }
+            /* Check me! */
             item.Checked = true;
+            /* Start timer */
             if (!timer.Enabled) timer.Start();
         }
 
-        public static Icon GetIcon()
+        public Icon GetIcon()
         {
-            var caps = WinApiCalls.QueryTimerResolution();
-            var number = (int)(caps.PeriodCurrent / 10000.0);
+            /* Query the timer resolution from Windows API */
+            TimerCaps caps = WinApiCalls.QueryTimerResolution();
+            int number = (int)(caps.PeriodCurrent / 10000.0);
 
+            /* Icon creation */
             Bitmap bitmap = new Bitmap(32, 32);
             Graphics graphics = Graphics.FromImage(bitmap);
 
@@ -78,11 +113,14 @@ namespace ClockResIcon
             graphics.DrawString(number.ToString(), font, new SolidBrush(Color.White), 16, 2, format);
             Icon createdIcon = Icon.FromHandle(bitmap.GetHicon());
 
+            /* Purge */
             fill.Dispose();
             font.Dispose();
+            format.Dispose();
             graphics.Dispose();
             bitmap.Dispose();
 
+            /* Icon */
             return createdIcon;
         }
     }
